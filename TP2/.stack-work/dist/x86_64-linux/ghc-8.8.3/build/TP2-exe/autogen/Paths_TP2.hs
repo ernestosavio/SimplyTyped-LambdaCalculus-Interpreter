f14b77deb286a298de36ce8168e62e472e399b2e
ernestosavio <ernesto_savio@hotmail.com>
2025-11-17 17:22:02 -0300

    exercise1 done - conversion from LamTerm to Term working

---
+++ TP2/.stack-work/dist/x86_64-linux/ghc-8.8.3/build/TP2-exe/autogen/Paths_TP2.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_TP2 (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/ernesto/Desktop/alp/tp2-repo/TP2/.stack-work/install/x86_64-linux/cb9da8a636ee6c449b3bff86079f425c9008e179388db81a87a352d3613dfa68/8.8.3/bin"
+libdir     = "/home/ernesto/Desktop/alp/tp2-repo/TP2/.stack-work/install/x86_64-linux/cb9da8a636ee6c449b3bff86079f425c9008e179388db81a87a352d3613dfa68/8.8.3/lib/x86_64-linux-ghc-8.8.3/TP2-0.1.0.0-2c9qde79sPOLVEqyS9Uqj7-TP2-exe"
+dynlibdir  = "/home/ernesto/Desktop/alp/tp2-repo/TP2/.stack-work/install/x86_64-linux/cb9da8a636ee6c449b3bff86079f425c9008e179388db81a87a352d3613dfa68/8.8.3/lib/x86_64-linux-ghc-8.8.3"
+datadir    = "/home/ernesto/Desktop/alp/tp2-repo/TP2/.stack-work/install/x86_64-linux/cb9da8a636ee6c449b3bff86079f425c9008e179388db81a87a352d3613dfa68/8.8.3/share/x86_64-linux-ghc-8.8.3/TP2-0.1.0.0"
+libexecdir = "/home/ernesto/Desktop/alp/tp2-repo/TP2/.stack-work/install/x86_64-linux/cb9da8a636ee6c449b3bff86079f425c9008e179388db81a87a352d3613dfa68/8.8.3/libexec/x86_64-linux-ghc-8.8.3/TP2-0.1.0.0"
+sysconfdir = "/home/ernesto/Desktop/alp/tp2-repo/TP2/.stack-work/install/x86_64-linux/cb9da8a636ee6c449b3bff86079f425c9008e179388db81a87a352d3613dfa68/8.8.3/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "TP2_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "TP2_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "TP2_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "TP2_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "TP2_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "TP2_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
---
+++ TP2/src/Simplytyped.hs
@@ -0,0 +1,121 @@
+module Simplytyped
+  ( conversion
+  ,    -- conversion a terminos localmente sin nombre
+    eval
+  ,          -- evaluador
+    infer
+  ,         -- inferidor de tipos
+    quote          -- valores -> terminos
+  )
+where
+
+import           Data.List
+import           Data.Maybe
+import           Prelude                 hiding ( (>>=) )
+import           Text.PrettyPrint.HughesPJ      ( render )
+import           PrettyPrinter
+import           Common
+
+-----------------------
+-- conversion
+-----------------------
+
+-- conversion a términos localmente sin nombres
+conversion :: LamTerm -> Term
+conversion (LVar str) = Free (Global str)
+conversion (LAbs str t term) = let term' = conversion term 
+                                  in (Lam t 
+                                      (bound2Brujin (Global str) 0 term'))
+
+conversion (LApp term1 term2) = let 
+                                  term1' = conversion term1
+                                  term2' = conversion term2
+                                in
+                                  term1' :@: term2'
+
+
+bound2Brujin :: Name -> Int -> Term -> Term
+bound2Brujin _ _ t@(Bound j) = t
+bound2Brujin idx i t@(Free idx') = if (idx == idx') 
+                                     then (Bound i)
+                                     else t                                
+
+bound2Brujin idx i (term1 :@: term2) = let
+                                         term1' = bound2Brujin idx i term1
+                                         term2' = bound2Brujin idx i term2
+                                       in
+                                         term1' :@: term2'
+
+bound2Brujin idx i (Lam t term) = Lam t (bound2Brujin idx (i+1) term)
+
+----------------------------
+--- evaluador de términos
+----------------------------
+
+-- substituye una variable por un término en otro término
+sub :: Int -> Term -> Term -> Term
+sub i t (Bound j) | i == j    = t
+sub _ _ (Bound j) | otherwise = Bound j
+sub _ _ (Free n   )           = Free n
+sub i t (u   :@: v)           = sub i t u :@: sub i t v
+sub i t (Lam t'  u)           = Lam t' (sub (i + 1) t u)
+
+-- convierte un valor en el término equivalente
+quote :: Value -> Term
+quote (VLam t f) = Lam t f
+
+-- evalúa un término en un entorno dado
+eval :: NameEnv Value Type -> Term -> Value
+eval = undefined
+
+
+
+
+----------------------
+--- type checker
+-----------------------
+
+-- infiere el tipo de un término
+infer :: NameEnv Value Type -> Term -> Either String Type
+infer = infer' []
+
+-- definiciones auxiliares
+ret :: Type -> Either String Type
+ret = Right
+
+err :: String -> Either String Type
+err = Left
+
+(>>=)
+  :: Either String Type -> (Type -> Either String Type) -> Either String Type
+(>>=) v f = either Left f v
+-- fcs. de error
+
+matchError :: Type -> Type -> Either String Type
+matchError t1 t2 =
+  err
+    $  "se esperaba "
+    ++ render (printType t1)
+    ++ ", pero "
+    ++ render (printType t2)
+    ++ " fue inferido."
+
+notfunError :: Type -> Either String Type
+notfunError t1 = err $ render (printType t1) ++ " no puede ser aplicado."
+
+notfoundError :: Name -> Either String Type
+notfoundError n = err $ show n ++ " no está definida."
+
+-- infiere el tipo de un término a partir de un entorno local de variables y un entorno global
+infer' :: Context -> NameEnv Value Type -> Term -> Either String Type
+infer' c _ (Bound i) = ret (c !! i)
+infer' _ e (Free  n) = case lookup n e of
+  Nothing     -> notfoundError n
+  Just (_, t) -> ret t
+infer' c e (t :@: u) = infer' c e t >>= \tt -> infer' c e u >>= \tu ->
+  case tt of
+    FunT t1 t2 -> if (tu == t1) then ret t2 else matchError t1 tu
+    _          -> notfunError tt
+infer' c e (Lam t u) = infer' (t : c) e u >>= \tu -> ret $ FunT t tu
+
+
